a26bb8f518a728514941e2c039870fc42c619c10
Kristian Freeman <kristian@kristianfreeman.com>
2018-10-03 17:48:04 -0500

    Links

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -12,9 +12,9 @@ class Bio extends React.Component {
           An explorative space, by Kristian Freeman. Kristian is a software
           developer, writer, and musician.{' '}
         </p>
-        <p>Doing: Byteconf - free developer conferences for everyone. </p>
-        <p>Formerly: Product Hunt, Simple.</p>
-        <p>Find me: Twitter, Soundcloud. </p>
+        <p>Doing: <a href="https://byteconf.com">Byteconf - free developer conferences for everyone</a>. </p>
+        <p>Formerly: <a href="https://producthunt.com">Product Hunt</a>, <a href="https://simple.com">Simple</a>.</p>
+        <p>Find me: <a href="https://twitter.com/imkmf">Twitter</a>, <a href="https://soundcloud.com">Soundcloud</a>.</p>
       </div>
     )
   }
